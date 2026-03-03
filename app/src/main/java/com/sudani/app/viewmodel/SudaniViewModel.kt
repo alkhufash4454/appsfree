a48c68ff3ec7f2d54b61ec48a70c1d810a4935e5
alkhufash <momenalfadel3@gmail.com>
2026-03-03 20:12:54 +0200

    Update SudaniViewModel.kt

--- app/src/main/java/com/sudani/app/viewmodel/SudaniViewModel.kt
+++ app/src/main/java/com/sudani/app/viewmodel/SudaniViewModel.kt
@@ -9,55 +9,55 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-// ... تعريف الـ UiState والـ ServiceOffering كما سبق
-
 class SudaniViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = SudaniRepository()
-    var dashboardData by mutableStateOf<DashboardData?>(null)
-    var fullUserData by mutableStateOf<OnboardingData?>(null)
+    
+    private val _uiState = MutableStateFlow<UiState>(UiState.Idle)
+    val uiState = _uiState.asStateFlow()
+
+    // المتغيرات المطلوبة في MainActivity والواجهات
+    var isLoggedIn by mutableStateOf(false)
+    var isOtpSent by mutableStateOf(false)
     var msisdn by mutableStateOf("")
+    var otp by mutableStateOf("")
     var token: String? = null
+    var fullUserData by mutableStateOf<OnboardingData?>(null)
+    var dashboardData by mutableStateOf<DashboardData?>(null)
+    var savedAccounts by mutableStateOf<List<OnboardingData>>(emptyList())
 
     val allServices = listOf(
         ServiceOffering("243586", "Ahla Youm", "Mixed", "2000", "100", "300", "1570"),
         ServiceOffering("244341", "10GB", "Data", "12500", "0", "10240", "2002")
-        // أضف البقية هنا من ملف الـ config.py الخاص بك
     )
 
-    fun fetchDashboard() {
-        val userData = fullUserData ?: return
+    fun sendOtp() {
         viewModelScope.launch {
-            try {
-                val response = repository.getDashboard(msisdn, token!!, userData)
-                if (response.isSuccessful) dashboardData = response.body()?.data
-            } catch (e: Exception) { }
+            _uiState.value = UiState.Loading
+            val res = repository.generateOtp(msisdn)
+            if (res.isSuccessful) isOtpSent = true
+            _uiState.value = UiState.Idle
         }
     }
 
-    fun claimPointsManual() {
-        val userData = fullUserData ?: return
-        val pts = dashboardData?.totalLoyaltyPoints?.split(".")?.get(0) ?: "0"
+    fun verifyOtp() {
         viewModelScope.launch {
-            repository.claimPoints(msisdn, token!!, userData, pts)
-            fetchDashboard()
+            _uiState.value = UiState.Loading
+            val res = repository.completeOnboarding(msisdn, otp)
+            if (res.isSuccessful) {
+                fullUserData = res.body()?.data
+                token = fullUserData?.token
+                isLoggedIn = true
+            }
+            _uiState.value = UiState.Idle
         }
     }
 
-    fun activateKhufashOffer(type: String) {
-        val userData = fullUserData ?: return
-        val pts = dashboardData?.totalLoyaltyPoints?.split(".")?.get(0) ?: "0"
-        viewModelScope.launch {
-            if (type == "300mb") repository.redeemPointsOffer(msisdn, token!!, userData, pts, "320196", "2002", "300")
-            else repository.redeemPointsOffer(msisdn, token!!, userData, pts, "320197", "2023", "1024")
-            fetchDashboard()
-        }
+    fun switchAccount(account: OnboardingData) {
+        fullUserData = account
+        msisdn = account.customerId ?: ""
+        token = account.token
+        isLoggedIn = true
     }
-    
-    fun subscribeWithBalance(service: ServiceOffering) {
-        val userData = fullUserData ?: return
-        viewModelScope.launch {
-            repository.subscribeToService(msisdn, token!!, userData, service)
-            fetchDashboard()
-        }
-    }
+
+    fun logout() { isLoggedIn = false }
 }
